c4af81f395aa5a7ca37b38ab0afe9a8b62e1ab5b
Emmanuel Tabares Ortiz <emanuel_tabares@hotmail.com>
2023-09-10 20:39:58 -0500

    Consumo de API proyecto apptower: modulo espacios

---
+++ Controllers/EspaciosController.cs
@@ -0,0 +1,55 @@
+﻿using Espacios.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Espacios.Controllers
+
+{
+    public class EspaciosController : Controller
+    {
+        private readonly HttpClient httpClient;
+        public EspaciosController()
+        {
+
+            httpClient = new HttpClient();
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var apiUrl = "https://apptower-bk.onrender.com/api/espacios";
+            var response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var espacios = JsonConvert.DeserializeObject<ApiResponse<MyEspacios>> (content);
+                return View(espacios.Results);
+            }
+
+            return View(new List<MyEspacios>());
+        }
+
+        public class ApiResponse<T>
+        {
+            public Info Info { get; set; }
+            public List<T> Results { get; set; }
+
+        }
+
+        public class Info
+        {
+
+            public int Count { get; set; }
+            public int Pages { get; set; }
+            public string Next { get; set; }
+            public string Prev { get; set; }
+
+        }
+    }
+
+    
+}
---
+++ Models/Class.cs
@@ -0,0 +1,33 @@
+﻿namespace Espacios.Models
+{
+    public class MyEspacios
+    {
+        public string _id { get; set; }
+        public string tipo_espacio { get; set; }
+        public string nombre_espacio { get; set; }
+        public int? area { get; set; }
+        public int? capacidad { get; set; }
+        public string estado { get; set; }
+        //public Origin Origin { get; set; }
+        //public Location Location { get; set; }
+
+    }
+
+    //public class Location
+    //{
+
+    //    public string Name { get; set; }
+    //    public string Url { get; set; }
+
+    //}
+
+    //public class Origin
+    //{
+
+    //    public string Name { get; set; }
+    //    public string Url { get; set; }
+
+    //}
+
+
+}
